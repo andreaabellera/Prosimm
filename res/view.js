cb30ba51745773bada3c77cb5815a035290b67e5
Andrea Abellera <58119036+andreaabellera@users.noreply.github.com>
2022-03-24 23:48:48 -0500

    Added skeleton and resources

---
+++ res/view.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    /*
+    document.getElementById("sth").addEventListener("click", function f(e) {
+        // Code
+    });
+    */
+   
+})
